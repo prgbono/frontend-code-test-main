f0138cb3e9aa14624af17fc243050fcd4326ddd2
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-10 13:14:57 +0200

    Complete PersonPage and add Footer

--- src/people/pages/PersonPage.tsx
+++ src/people/pages/PersonPage.tsx
@@ -1,5 +1,32 @@
-const PersonPage = () => {
-  return <div>Person</div>
+// FIXME: Remove type to another file. Reuse it in PeopleCard component
+import Footer from '../../ui/Footer'
+import { Person } from '../types'
+const PersonPage = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters
+}: Person) => {
+  return (
+    <div className="flex flex-col items-center min-h-screen p-4">
+      <h1 className="text-2xl font-bold mb-12">Person 1 Name</h1>
+      <p className="mb-12">
+        This is PersonPAge List of producers he has worked with and how many
+        times
+      </p>
+      <p className="mb-4">Species Average Height</p>
+      <h2 className="text-xl mb-2">Films</h2>
+      <div className="flex flex-col items-center border p-4 mb-4 w-1/3">
+        <h3 className="text-lg font-bold">Title</h3>
+        <p className="mb-10">Release Date</p>
+        <p>Number of planets without water</p>
+      </div>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default PersonPage
---
+++ src/ui/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-between w-1/3">
+      <button className="border p-2">Prev</button>
+      <button className="border p-2">Next</button>
+    </footer>
+  )
+}
+
+export default Footer
