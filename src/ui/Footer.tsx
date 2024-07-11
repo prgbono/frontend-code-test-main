124834397ad8f634d4b70616afbb2366e4b05a68
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-11 12:59:19 +0200

    feat: :bulb: Add go to Home button

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom'
+
 const Footer = () => {
+  const navigate = useNavigate()
+  const onNavigateBack = () => {
+    navigate(-1)
+  }
   return (
     <footer className="flex justify-between w-1/3">
-      <button className="border p-2">Prev</button>
-      <button className="border p-2">Next</button>
+      <button className="p-2 border">Prev</button>
+      <button className="p-2 border" onClick={onNavigateBack}>
+        Home
+      </button>
+      <button className="p-2 border">Next</button>
     </footer>
   )
 }
