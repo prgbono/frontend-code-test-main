0c3153ad4affc4ea24b1a98d6a76ce158c10e171
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-11 17:22:46 +0200

    refactor: :construction: Working on queries

--- src/people/queries/index.ts
+++ src/people/queries/index.ts
@@ -9,9 +9,17 @@ export const GET_PERSON_BY_ID_QUERY = gql`
       species {
         averageHeight
       }
+      filmConnection {
+        films {
+          producers
+          title
+          releaseDate
+        }
+      }
     }
   }
 `
+
 export const ALL_PEOPLE_QUERY = gql`
   query Home {
     allPeople {
@@ -24,3 +32,65 @@ export const ALL_PEOPLE_QUERY = gql`
     }
   }
 `
+
+export const GET_PLANETS_BY_FILM_ID_QUERY = gql`
+  query getPlanetsByFilmId($filmId: ID!) {
+    film(id: $filmId) {
+      id
+      planetConnection {
+        planets {
+          surfaceWater
+          name
+        }
+      }
+    }
+  }
+`
+
+/* TODO: 
+Esta consulta te da los planetas que hay en la pelicúla con ese film id. 
+Ekemplo de respueta para este id de film:
+query getPlanetsByFilmId($filmId: ID!) {
+film(id: "ZmlsbXM6Mg==") {
+      id
+      planetConnection {
+        planets {
+          surfaceWater
+          name
+        }
+      }
+      
+  }
+}
+
+REspuesta
+{
+  "data": {
+    "film": {
+      "id": "ZmlsbXM6Mg==",
+      "planetConnection": {
+        "planets": [
+          {
+            "surfaceWater": 100,
+            "name": "Hoth"
+          },
+          {
+            "surfaceWater": 8,
+            "name": "Dagobah"
+          },
+          {
+            "surfaceWater": 0,
+            "name": "Bespin"
+          },
+          {
+            "surfaceWater": 10,
+            "name": "Ord Mantell"
+          }
+        ]
+      }
+    }
+  }
+}
+
+
+*/
