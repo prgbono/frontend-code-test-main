b012fa41892bcbc2728fb5ca77c897e53ccd221b
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-12 17:25:06 +0200

    perf: :bug: Improve goToHome button

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -2,14 +2,14 @@ import { useNavigate } from 'react-router-dom'
 
 const Footer = () => {
   const navigate = useNavigate()
-  const onNavigateBack = () => {
-    navigate(-1)
+  const navigateToHome = () => {
+    navigate('/')
   }
   return (
     <footer className="w-1/3 mt-4">
       <button
         className="w-full p-2 px-4 py-2 font-bold border"
-        onClick={onNavigateBack}
+        onClick={navigateToHome}
       >
         Home
       </button>
