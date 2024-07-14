499523de50a030f09c1f3c7686eb2f871d10ba04
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-14 09:19:48 +0200

    feat: :art: Format data service

---
+++ src/people/helpers/formatDate.ts
@@ -0,0 +1,6 @@
+// FIXME: typos
+export const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  const date = new Date(dateString)
+  return date.toLocaleDateString('en-US', options)
+}
